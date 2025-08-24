f6a40c1590c383c2b3994ded995616ab8b841945
Techie 86 <shyamdancer@gmail.com>
2025-08-25 02:12:21 +0530

    code updated for data reading using pandas

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -1,5 +1,23 @@
-from langchain_astradb import AstraDBVectorStore
+import langchain_astradb
 from dotenv import load_dotenv
 import os
 import pandas as pd
 from data_ingestion.data_transform import data_converter
+
+
+class ingest_data:
+    def __init__(self):
+        pass
+    
+    
+    def data_ingestion(self):
+        print("Data ingestion started!")
+    
+    
+if __name__ == '__main__':
+    print("Running data_ingest.py...")
+    
+    data_ingestion=ingest_data()
+    data_ingestion.data_ingestion()
+    # your code heredata_ingestion = ingest_data()
+
